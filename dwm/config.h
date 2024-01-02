19dc04ce351243b687412b59b9b379e4663bacc8
Next-Token <sethserpas@icloud.com>
2024-01-02 17:21:13 -0600

    slstatus shows cpu/ram% and time

--- dwm/config.h
+++ dwm/config.h
@@ -51,7 +51,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5" };
+static const char *tags[] = { "1", "2", "3", "4", "5", ""};
 
 static const Rule rules[] = {
 	/* xprop(1):
